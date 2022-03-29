84316ae2d2d3c5cb88e8fb14e185f0748ed36ee5
58peiShan <99083443+58peiShan@users.noreply.github.com>
2022-03-29 18:51:45 +0800

    add best score function

--- game.js
+++ game.js
@@ -3,14 +3,13 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 //存取玩家選取的按鈕順序
 let userClickedPattern = [];
-
 //點擊按鈕動畫
 function animatePress(currentColour) {
     $("#" + currentColour).addClass("pressed");
     setTimeout(function () {
         $("#" + currentColour).removeClass("pressed");
     }, 100);
-    console.log(`我的順序[${userClickedPattern}]`);
+    //console.log(`我的順序[${userClickedPattern}]`);
 };
 function playSound(color) {
     let audio = new Audio(`../sounds/${color}.mp3`);
@@ -32,15 +31,15 @@ $(document).on("keydown", function () {
 $(".btn").on("click", function () {
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    console.log(`我的click:${userChosenColour}`);
     animatePress(userChosenColour);
     checkAnswer(userClickedPattern.length - 1);
-    //播放與使用者點擊按鈕相符的音效
     playSound(userChosenColour);
+    //console.log(`我的順序${userClickedPattern}`);
 });
 
 function checkAnswer(currentLevel) {
     if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
-        console.log("成功");
         if (userClickedPattern.length === gamePattern.length) {
             setTimeout(function () {
                 nextSequence();
@@ -58,24 +57,37 @@ function checkAnswer(currentLevel) {
 };
 
 //隨機選擇按鈕
+let record = [];
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text(`Level ${level}`);
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    console.log(`遊戲順序[${gamePattern}]`);
     //閃爍
     $('#' + randomChosenColour).fadeOut(100).fadeIn(100);
     //播放與隨機按鈕相符的音效
     playSound(randomChosenColour);
-    level++;
-    $("#level-title").text(`Level ${level}`);
-    userClickedPattern = [];
+    //console.log(`遊戲順序${gamePattern}`);
+    record.push(level);
+    
 };
 
-//Restart
-function startOver(){
+
+//重新開始
+function startOver() {
     $("#level-title").text(`Game Over, Press Any Key to Restart`);
     pressed = false;
     gamePattern = [];
-    level=0;
-}
+    level = 0;
+    //最佳紀錄
+    record.sort(function (a, b) {
+        return b - a 
+    });
+    if(record.length>0){
+        $("h2").text(`Best:level ${record[0]-1}`);
+    }else{
+        $("h2").text(`Best:level 0`);
+    }
+};
